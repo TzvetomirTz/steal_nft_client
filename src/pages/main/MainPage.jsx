3975931e660bf8b13a017c2d99de7ecc0dc3e818
TzvetomirTz <cvetomiriv.cvetkov@gmail.com>
2024-05-16 16:29:55 +0300

    Initialize connect wallet panel

--- src/pages/main/MainPage.jsx
+++ src/pages/main/MainPage.jsx
@@ -1,19 +1,38 @@
+import { useState } from 'react';
 import TopBar from '../../components/topBar/TopBar';
 import './MainPage.css';
+import metamaskIcon from '../../assets/wlt_ics/mtmsk_icn.svg'
 
 const MainPage = () => {
+	const [displayMainPageBanner, setDisplayMainPageBanner] = useState(true);
+	const [displayMainPageConnectWalletPanel, setDisplayMainPageConnectWalletPanel] = useState(false);
+	const [displayMainPageStealPanel, setDisplayMainPageStealPanel] = useState(false);
+
+	const onStart = () => {
+		setDisplayMainPageBanner(false);
+		setDisplayMainPageConnectWalletPanel(true);
+	};
+
 	return (
 		<div className='MainPage'>
 			<TopBar />
 			<div className='MainPageBody'>
-				<div className='MainPageBanner'>
+				{displayMainPageBanner && <div className='MainPageBanner'>
 					<div className='MainPageBannerTitle NoSelect'>StealNFT</div>
 					<div className='MainPageBannerSubtext  NoSelect'>
 						When “right click” + “save image as” just doesn’t do it for you anymore...
 					</div>
-					<div className='MainPageBannerStartButton  NoSelect'>Start</div>
-				</div>
-				{/* <div className='MainPageMidPanel'> </div> */}
+					<div className='MainPageBannerStartButton  NoSelect' onClick={ onStart }>Start</div>
+				</div>}
+				{displayMainPageConnectWalletPanel && <div className='MainPageConnectWalletPanel'>
+					<div className='MainPageConnectWalletPanelTitle NoSelect'>Connect Wallet</div>
+					<div className='MainPageConnectWalletList'>
+						<div className='WalletOption'>
+							<img src={ metamaskIcon } alt='' className='WalletIcon NoSelect' />
+							<div className='WalletOptionTitle NoSelect'>MetaMask</div>
+						</div>
+					</div>
+				</div>}
 			</div>
 		</div>
 	);
