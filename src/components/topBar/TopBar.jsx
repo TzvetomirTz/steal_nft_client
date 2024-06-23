2fe8915f53bd642efd323ae4dcb3092e293ae069
TzvetomirTz <cvetomiriv.cvetkov@gmail.com>
2024-06-24 00:02:58 +0300

    Fix navigation triggering a warn log

--- src/components/topBar/TopBar.jsx
+++ src/components/topBar/TopBar.jsx
@@ -1,16 +1,21 @@
 import './TopBar.css';
+import { useNavigate } from 'react-router-dom';
+import { useCallback } from 'react';
 
 const TopBar = () => {
+	const navigate = useNavigate();
+	const goToMainPage = useCallback(() => { navigate('/'); });
+	const goToFaqPage = useCallback(() => { navigate('/faq'); });
+
 	return (
 		<div className='TopBar'>
 			<div className='TopBarLeft'>
-				<div className='TopBarLogoText NoSelect'>YoinkNFT</div>
+				<div className='TopBarLogoText NoSelect' onClick={goToMainPage}>YoinkNFT</div>
 			</div>
 			<div className='TopBarRight'>
-				<div className='TopBarButton NoSelect'>FAQ</div>
-				<div className='TopBarButton NoSelect'>Rights</div>
+				<div className='TopBarButton NoSelect' onClick={goToFaqPage}>FAQ</div>
 				<div className='TopBarButton NoSelect'>About</div>
-				<div className='TopBarButton NoSelect'>Explore</div>
+				<div className='TopBarButton NoSelect'>Explore NFTs ↗</div>
 			</div>
 		</div>
 	);
