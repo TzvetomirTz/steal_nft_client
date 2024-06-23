56050ee6a5ae5db4c245193794615b65c8309976
TzvetomirTz <cvetomiriv.cvetkov@gmail.com>
2024-06-24 00:28:46 +0300

    Add some important info into the FAQ page

--- src/pages/faq/FaqPage.jsx
+++ src/pages/faq/FaqPage.jsx
@@ -5,6 +5,12 @@ const FaqPage = () => {
 	return (
 		<div className='FaqPage'>
 			<TopBar />
+            <div className='FaqPageBody'>
+                <div className='QuestionAnswerWrapper'>
+                    <div className='Question NoSelect'>► My Yoink request doesn't work</div>
+                    <div className='Answer NoSelect'>Have you tried [Right Click] + [Save Image As] instead?</div>
+                </div>
+            </div>
 		</div>
 	);
 }
